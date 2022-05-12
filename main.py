f8517f3c37ec855126fb04b4d950afe43337a523
Zedoman <91881492+Zedoman@users.noreply.github.com>
2022-05-12 20:47:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+918597388027', 'hey!', 9, 30)
+#pywhatkit.sendwhatmsg_instantly('+918597388027', 'hi')
+#pywhatkit.sendwhatmsg_to_group('https://chat.whatsapp.com/BEWqFvkxcUG0F8YtbUV7h5', 'hi', 11,53)
+print("successful")
